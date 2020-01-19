f8bf2f26d2703333fd663fb9d4958b1f63460be3
aNiL <aNiL@aNiL-PC>
2020-01-19 17:21:17 +0530

    area triangle

--- src/main/java/com/startup/service/CalculationService.java
+++ src/main/java/com/startup/service/CalculationService.java
@@ -2,4 +2,5 @@ package com.startup.service;
 
 public interface CalculationService {
 
+ public void areaTrangle(double height, double base);
 }
--- src/main/java/com/startup/serviceimpl/CalculationServiceImpl.java
+++ src/main/java/com/startup/serviceimpl/CalculationServiceImpl.java
@@ -4,4 +4,10 @@ import com.startup.service.CalculationService;
 
 public class CalculationServiceImpl implements CalculationService {
 
+	public void areaTrangle(double height, double base) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 }
