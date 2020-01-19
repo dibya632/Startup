a8f74cd8efae19978cfec28c3f5e88f412a09782
aNiL <aNiL@aNiL-PC>
2020-01-19 17:31:01 +0530

    Main method for Area of triangle

--- src/main/java/com/startup/serviceimpl/CalculationServiceImpl.java
+++ src/main/java/com/startup/serviceimpl/CalculationServiceImpl.java
@@ -4,6 +4,11 @@ import com.startup.service.CalculationService;
 
 public class CalculationServiceImpl implements CalculationService {
 
+	/**
+	 * method for area of triangle
+	 * @param height
+	 * @param base
+	 */
 	public void areaTrangle(double height, double base) {
 		// TODO Auto-generated method stub
 		
