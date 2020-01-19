3f5d67a77b2946d188c270c6c00ef1c81283d241
admin <admin@192.168.2.18>
2020-01-19 17:45:18 +0530

    File Added Moduler

--- src/main/java/com/startup/service/CalculationService.java
+++ src/main/java/com/startup/service/CalculationService.java
@@ -1,7 +1,17 @@
 package com.startup.service;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface CalculationService.
+ */
 public interface CalculationService {
 
+ /**
+  * Area trangle.
+  *
+  * @param height the height
+  * @param base the base
+  */
  public void areaTrangle(double height, double base);
- 
+
 }
