4ec25f4e5fd9f109b86c37fd410e9447c09bfa4f
sumesh <sumesh@192.168.2.19>
2020-01-19 17:38:32 +0530

    adding multiplication main method.

---
+++ src/main/java/com/startup/main/Multiplication.java
@@ -0,0 +1,10 @@
+package com.startup.main;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/main/java/com/startup/service/CalculationService.java
+++ src/main/java/com/startup/service/CalculationService.java
@@ -2,11 +2,6 @@ package com.startup.service;
 
 public interface CalculationService {
 
-	/**
-	 * Author: Anil
-	 * Abstract method for Area of Triangle
-	 * @param height
-	 * @param base
-	 */
  public void areaTrangle(double height, double base);
+ 
 }
